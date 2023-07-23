a108ff9a426a5e33d854864b05c32ab8c1157554
Karuthadath <karuthadath@hpe.com>
2023-07-23 18:43:29 +0530

    added static part in voting

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import json
-from flask import Flask, render_template, request, session, redirect, url_for
+from flask import Flask, render_template, request, session, redirect, url_for, send_from_directory
 import os
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
+app.config['STATIC_FOLDER'] = 'static'  # Configuring the static folder
 
 candidates = {
     "Noushad": 0,
@@ -23,11 +24,17 @@ def index():
                 candidates[candidate] += 1
                 session["voted"] = True
         return redirect(url_for("results"))
-    return render_template("index.html", candidates=candidates, slogans=slogans)
+    return render_template("index.html", slogans=slogans)
 
 @app.route("/results")
 def results():
     return render_template("results.html", candidates=candidates)
+
+# Route to serve static files
+@app.route('/static/<path:filename>')
+def serve_static(filename):
+    root_dir = os.path.abspath(os.path.dirname(__file__))
+    return send_from_directory(os.path.join(root_dir, app.config['STATIC_FOLDER']), filename)
 
 if __name__ == "__main__":
     app.run()
