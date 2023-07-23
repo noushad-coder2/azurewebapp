46cfbea42837c0b25182f3021243c8fcd5235fd7
Karuthadath <karuthadath@hpe.com>
2023-07-23 16:27:33 +0530

    newlyadded

--- app.py
+++ app.py
@@ -1,19 +1,33 @@
-# app.py
-from flask import Flask, render_template, request
+import json
+from flask import Flask, render_template, request, session, redirect, url_for
+import os
 
 app = Flask(__name__)
+app.secret_key = os.urandom(24)
 
-names = []
+candidates = {
+    "Noushad": 0,
+    "Sahana": 0,
+}
 
-@app.route('/')
+# Load slogans from JSON file
+with open("slogans.json", "r") as f:
+    slogans = json.load(f)
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template('index.html', names=names)
+    if request.method == "POST":
+        if "voted" not in session:
+            candidate = request.form["candidate"]
+            if candidate in candidates:
+                candidates[candidate] += 1
+                session["voted"] = True
+        return redirect(url_for("results"))
+    return render_template("index.html", candidates=candidates, slogans=slogans)
 
-@app.route('/save', methods=['POST'])
-def save_name():
-    name = request.form['name']
-    names.append(name)
-    return render_template('index.html', names=names)
+@app.route("/results")
+def results():
+    return render_template("results.html", candidates=candidates)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
